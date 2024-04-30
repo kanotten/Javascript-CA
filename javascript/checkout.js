8e9c057ef56d907f49a4d56f8e2403c6bf8989a8
jakob <jakobhkrohn@gmail.com>
2024-04-30 18:46:12 +0200

    fix checkout.js

--- javascript/checkout.js
+++ javascript/checkout.js
@@ -1,32 +1,37 @@
-const checkoutHtml = document.getElementById("checkout");
-const totalPrice = document.getElementById("total");
-const checkoutArray = JSON.parse(localStorage.getItem("cart")) || [];
-const pay = document.getElementById("goToPayment");
+document.addEventListener("DOMContentLoaded", async function () {
 
-function cart() {
-  if (!checkoutArray || checkoutArray.length === 0) {
-    checkoutHtml.innerHTML = "<h2>Cart is empty</h2>";
-    return;
-  }
-  checkoutArray.forEach((movie) => {
-    checkoutHtml.innerHTML += `
+  function cart() {
+    if (!checkoutArray || checkoutArray.length === 0) {
+      checkoutHtml.innerHTML = "<h2>Cart is empty</h2>";
+      return;
+    }
+    checkoutArray.forEach((movie) => {
+      checkoutHtml.innerHTML += `
             <div>${movie.title}</div>
             <div>${movie.price}</div>
             <div>${movie.quantity}</div>
             <img class="checkoutImg" src="${movie.image}" alt="${movie.title}">
             `;
-  });
-}
-function renderCart() {
-  let totalSum = 0;
+    });
+  }
+  function renderCart() {
+    let totalSum = 0;
 
-  checkoutArray.forEach((item) => {
-    totalSum += item.price * item.quantity;
+    checkoutArray.forEach((item) => {
+      totalSum += item.price * item.quantity;
+    });
+    totalPrice.innerHTML = `<h3>total: ${totalSum}</h3>`;
+  }
+
+  const checkoutHtml = document.getElementById("checkout");
+  const totalPrice = document.getElementById("total");
+  const checkoutArray = JSON.parse(localStorage.getItem("cart")) || [];
+  const pay = document.getElementById("goToPayment");
+
+  pay.addEventListener("click", () => {
+    location.href = "../confirmation.html";
   });
-  totalPrice.innerHTML = `<h3>total: ${totalSum}</h3>`;
-}
-pay.addEventListener("click", () => {
-  location.href = "../confirmation.html";
+
+  renderCart();
+  cart();
 });
-renderCart();
-cart();
